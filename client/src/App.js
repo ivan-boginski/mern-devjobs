a978869586bd842dc636c91d291a07c1ebe57997
ivan-boginski <aisivan36@gmail.com>
2020-06-11 05:41:30 +0700

    added more bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
@@ -6,14 +7,18 @@ import Landing from "./components/layout/Landing";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-      <Footer />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
