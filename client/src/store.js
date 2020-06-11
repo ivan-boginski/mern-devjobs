683b0079111da997b239b50b76214a7e1ad70d1a
ivan-boginski <aisivan36@gmail.com>
2020-06-11 15:06:53 +0700

    added some code

---
+++ client/src/store.js
@@ -0,0 +1,8 @@
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+
+const middleware = [thunk];
+
+const store = createStore(() => [], {}, applyMiddleware(...middleware));
+
+export default store;
