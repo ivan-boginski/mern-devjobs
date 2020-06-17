82b5958bc4425042d525bd2cc6749dac8cbafc4c
ivan-boginski <aisivan36@gmail.com>
2020-06-17 07:37:01 +0700

    some fixed and added more as well

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/create-profile/CreateProfile";
 
 import store from "./store";
 import { clearCurrentProfile } from "./actions/profileActions";
@@ -54,6 +55,13 @@ class App extends Component {
               <Route exact path="/login" component={Login} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
               </Switch>
             </div>
             <Footer />
