a41c320a52dcdef33d74c3a06a75dd0874c35bce
ivan-boginski <aisivan36@gmail.com>
2020-06-17 07:18:14 +0700

    Added some bug to fix later

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
@@ -10,7 +10,12 @@ import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Dashboard from "./components/dashboard/Dashboard";
+
 import store from "./store";
+import { clearCurrentProfile } from "./actions/profileActions";
+
+import PrivateRoute from "./components/common/PrivateRoute";
 
 import "./App.css";
 
@@ -28,7 +33,9 @@ if (localStorage.jwtToken) {
   if (decoded.exp < currentTime) {
     /// Logout user
     store.dispatch(logoutUser());
-    // Todo Clear current Profile
+    // Clear current Profile
+    store.dispatch(clearCurrentProfile());
+
     // Redirect to login
     window.location.href = "/login";
   }
@@ -45,6 +52,9 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
             </div>
             <Footer />
           </div>
